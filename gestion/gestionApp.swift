e082da5c7355b9a12d53bfae92763d040e99b213
Mathieu Perez <mathieuperez07@gmail.com>
2026-04-26 16:55:44 +0200

    Initial Commit

---
+++ gestion/gestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  gestionApp.swift
+//  gestion
+//
+//  Created by Mathieu Perez on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct gestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
